549a661e1eac3cd28654d0132f9ebef2d03a48d7
Victor Ubak <victorben139@gmail.com>
2023-03-17 17:57:38 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,27 +9,9 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+	putchar(ch);
 	}
 	putchar('\n');
 	return (0);
-}
-
-3. alphABET
-#include <stdio.h>
-/**
- * main -  [WRITE A SHORT DESCRIPTION HERE]
- * Return: 0
- */
-int main(void)
-{
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-	putchar('\n');
-		return (0);
 }
 
