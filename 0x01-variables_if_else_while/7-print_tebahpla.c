e84ac7f1373129d6c242be862cf339137755bf50
Victor Ubak <victorben139@gmail.com>
2023-03-17 20:46:29 -0700

    print7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  [WRITE A SHORT DESCRIPTION HERE]
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
