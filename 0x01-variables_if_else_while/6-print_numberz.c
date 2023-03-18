10a180d95a417a7a01c081a9651df65a41f60d46
Victor Ubak <victorben139@gmail.com>
2023-03-17 20:38:55 -0700

    print6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - [WRITE A SHORT DESCRIPTION HERE]
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
