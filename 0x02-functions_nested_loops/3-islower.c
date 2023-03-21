08e7f2f7519feb53170e089ed94cf089899c55fa
Victor Ubak <victorben139@gmail.com>
2023-03-21 15:38:33 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - lower
+ * @c: c is lowercase
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
