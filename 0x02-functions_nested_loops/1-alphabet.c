32766ed45c5a17d7adae956a1efe9c3336787464
Victor Ubak <victorben139@gmail.com>
2023-03-21 13:58:25 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#iclude "main.h"
+/**
+ * print_alphabet - print alphabet
+ */
+void print_alphabet(void)
+{
+char letters = 'a'
+	while (letters <= 'z')
+	{
+	_putchar(letters);
+	letters++
+	}
+_putchar('\n');
+}
+
