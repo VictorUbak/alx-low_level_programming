4daa5c53f44d8738d48ad09a1d4c472142abdb95
Victor Ubak <victorben139@gmail.com>
2023-04-29 16:57:54 -0700

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
+
 /**
- * free_listint - free the list
+ * free_listint - free end list
  * @head: free list
  */
 void free_listint(listint_t *head)
 {
-listint_t *lead;
+	listint_t *tom;
 
-while (head != NULL)
-
-{
-head = lead;
-lead = head->next;
-free(head);
-}
+	while (head)
+	{
+		tom = head->next;
+		free(head);
+		head = tom;
+	}
 }
