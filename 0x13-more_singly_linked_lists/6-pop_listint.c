bca71d613537a3c270d1bd43a4b643631729eaa1
Victor Ubak <victorben139@gmail.com>
2023-04-30 13:39:25 -0700

    delete data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head node of a list
+ * @head: delete the head
+ * Return: n
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *lead;
+int pop = 0;
+
+	if (head == NULL || *head == NULL)
+	return (0);
+lead = *head;
+pop = lead->n;
+*head = (*head)->next;
+free(lead),
+	return (pop);
+}
