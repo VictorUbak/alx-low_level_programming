5c7a6115c02e767ca5dd36ed35542152888e7bbd
Victor Ubak <victorben139@gmail.com>
2023-05-04 20:50:58 +0100

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bat;
 
-	if (i > 63)
+	if (index > 63)
 		return (-1);
 
-	bat = (n >> i) & 1;
+	bat = (n >> index) & 1;
 
 	return (bat);
 }
